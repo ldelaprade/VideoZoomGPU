a55c2f2ecc6a8ae189e113b1e1fce87179b47192
Laurent de Laprade <ldelaprade@appareo.com>
2025-12-01 10:43:56 +0100

    Fix bug was spawning 2 UIs on start

--- EasyBuild.cmd
+++ EasyBuild.cmd
@@ -1,3 +1,5 @@
  :: Create a single-file, self-contained executable for Windows x64
  fxc.exe /T ps_2_0 /E main /Fo CropEffect.ps CropEffect.fx
- dotnet publish -c Release -r win-x64 /p:AllowUnsafeBlocks=true --self-contained true /p:PublishSingleFile=true /p:IncludeAllContentForSelfExtract=true
+ dotnet publish -c Release -r win-x64 /p:AllowUnsafeBlocks=true --self-contained true ^
+        /p:PublishSingleFile=true /p:IncludeAllContentForSelfExtract=true ^
+        /p:SatelliteResourceLanguages=en-US
